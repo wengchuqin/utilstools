2d3594d0bc54c400198b0d18c1b6dc04aed9b4a4
wengchuqin <wengchuqin@meituan.com>
2018-04-27 15:42:41 +0800

    alibaba fastjson简单使用

---
+++ json/src/test/java/top/chuqin/utils/tools/json/CommonDictPo.java
@@ -0,0 +1,34 @@
+package top.chuqin.utils.tools.json;
+
+public class CommonDictPo extends BasePo<Long>{
+    private String categoryCode;
+    private String categoryName;
+
+    public CommonDictPo() {
+        super();
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "CommonDictPo{" +
+                "categoryCode='" + categoryCode + '\'' +
+                ", categoryName='" + categoryName + '\'' +
+                "} " + super.toString();
+    }
+}
